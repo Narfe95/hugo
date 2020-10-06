e253fe5e3202b2515c393ae8447125d9195af255
Narfe95 <Narfe95@users.noreply.github.com>
2020-10-06 21:31:30 +0200

    asdf

--- go-hue.go
+++ go-hue.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"github.com/getlantern/systray"
+	"github.com/getlantern/systray/example/icon"
 	"github.com/gotk3/gotk3/gtk"
 	"log"
 )
@@ -13,56 +15,56 @@ var VPanel *gtk.Paned
 var Button *gtk.Button
 
 func main() {
-	daemon := ParseFlags()
-	gtk.Init(nil)
-
-	// Window
-	Window, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-	if err != nil {
-		log.Fatal("Unable to create window:", err)
-	}
-	Window.SetTitle("Go-Hue!")
-	Window.Connect("destroy", func() {
-		gtk.MainQuit()
-	})
-	//win.Connect("focus-in", window_focused)
-	//win.Connect("focus-out", window_unfocused)
-	Window.SetDefaultSize(800, 600)
-
-	// Label
-	Label, err = gtk.LabelNew("Test")
-	if err != nil {
-		log.Fatal("Unable to create label:", err)
-	}
-
-	// Entry
-	Entry, err = gtk.EntryNew()
-	if err != nil {
-		log.Fatal("Unable to create entry:", err)
-	}
-
-	Button, err = gtk.ButtonNew()
-	if err != nil {
-		log.Fatal("Unable to create button:", err)
-	}
-	Button.SetLabel("Run")
-	Button.Connect("clicked", button_onclick)
-
-	// horizontal panel: hPanel
-	VPanel, err = gtk.PanedNew(gtk.ORIENTATION_VERTICAL)
-	if err != nil {
-		log.Fatal("Unable to create panel:", err)
-	}
-	VPanel.Add1(Entry)
-	VPanel.Add2(Button)
-
-	Window.Add(VPanel)
-
-	if !daemon {
-		Window.ShowAll()
-	}
-
-	gtk.Main()
+	//daemon := ParseFlags()
+	systray.Run(onReady, onExit)
+	//gtk.Init(nil)
+	//
+	//// Window
+	//Window, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	//if err != nil {
+	//	log.Fatal("Unable to create window:", err)
+	//}
+	//Window.SetTitle("Go-Hue!")
+	//Window.Connect("destroy", func() {
+	//	gtk.MainQuit()
+	//})
+	//Window.SetDefaultSize(800, 600)
+	//
+	//// Label
+	//Label, err = gtk.LabelNew("Test")
+	//if err != nil {
+	//	log.Fatal("Unable to create label:", err)
+	//}
+	//
+	//// Entry
+	//Entry, err = gtk.EntryNew()
+	//if err != nil {
+	//	log.Fatal("Unable to create entry:", err)
+	//}
+	//
+	//Button, err = gtk.ButtonNew()
+	//if err != nil {
+	//	log.Fatal("Unable to create button:", err)
+	//}
+	//Button.SetLabel("Run")
+	//Button.SetSizeRequest(120, 18)
+	//Button.Connect("clicked", button_onclick)
+	//
+	//// horizontal panel: hPanel
+	//VPanel, err = gtk.PanedNew(gtk.ORIENTATION_VERTICAL)
+	//if err != nil {
+	//	log.Fatal("Unable to create panel:", err)
+	//}
+	//VPanel.Add1(Entry)
+	//VPanel.Add2(Button)
+	//
+	//Window.Add(VPanel)
+	//
+	//if !daemon {
+	//	Window.ShowAll()
+	//}
+	//
+	//gtk.Main()
 }
 
 func ParseFlags() bool {
@@ -84,4 +86,18 @@ func button_onclick() {
 	dialog.SetTitle(dialogText)
 	dialog.SetDefaultSize(200, 100)
 	dialog.Show()
+}
+
+func onReady() {
+	systray.SetIcon(icon.Data)
+	systray.SetTitle("Awesome App")
+	systray.SetTooltip("Pretty awesome")
+	_ = systray.AddMenuItem("Quit", "Quit the whole app")
+	//go func() {
+	//	<-quitItem.ClickedCh
+	//}
+}
+
+func onExit() {
+	systray.Quit()
 }
