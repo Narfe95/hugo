3329cc75879943fb5ee9243e1f4a5b526a216b97
Narfe95 <Narfe95@users.noreply.github.com>
2020-10-07 22:49:03 +0200

    DNS_PROBE_FINISHED_NXDOMAIN

--- go-hue.go
+++ go-hue.go
@@ -3,160 +3,84 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
-
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
 	"github.com/getlantern/systray"
-	"github.com/getlantern/systray/example/icon"
-	"github.com/gotk3/gotk3/gtk"
+	"io/ioutil"
+	"log"
 )
 
-var Window *gtk.Window
-var Label *gtk.Label
-var Entry *gtk.Entry
-var VPanel *gtk.Paned
-var Button *gtk.Button
+var applicationName = "Go-hue!"
+var daemon bool
+
+var application fyne.App
+var window fyne.Window
+var label *widget.Label
+var button *widget.Button
 
 func main() {
-	//daemon := ParseFlags()
-	systray.Run(onReady, onExit)
-	//gtk.Init(nil)
-	//
-	//// Window
-	//Window, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-	//if err != nil {
-	//	log.Fatal("Unable to create window:", err)
-	//}
-	//Window.SetTitle("Go-Hue!")
-	//Window.Connect("destroy", func() {
-	//	gtk.MainQuit()
-	//})
-	//Window.SetDefaultSize(800, 600)
-	//
-	//// Label
-	//Label, err = gtk.LabelNew("Test")
-	//if err != nil {
-	//	log.Fatal("Unable to create label:", err)
-	//}
-	//
-	//// Entry
-	//Entry, err = gtk.EntryNew()
-	//if err != nil {
-	//	log.Fatal("Unable to create entry:", err)
-	//}
-	//
-	//Button, err = gtk.ButtonNew()
-	//if err != nil {
-	//	log.Fatal("Unable to create button:", err)
-	//}
-	//Button.SetLabel("Run")
-	//Button.SetSizeRequest(120, 18)
-	//Button.Connect("clicked", button_onclick)
-	//
-	//// horizontal panel: hPanel
-	//VPanel, err = gtk.PanedNew(gtk.ORIENTATION_VERTICAL)
-	//if err != nil {
-	//	log.Fatal("Unable to create panel:", err)
-	//}
-	//VPanel.Add1(Entry)
-	//VPanel.Add2(Button)
-	//
-	//Window.Add(VPanel)
-	//
-	//if !daemon {
-	//	Window.ShowAll()
-	//}
-	//
-	//gtk.Main()
+	ParseFlags()
+	fyneWindow()
+	//systray.Run(onReady, onExit)
 }
 
-func ParseFlags() bool {
-	var daemon bool
+func fyneWindow() {
+	// Create application
+	application = app.New()
+	// Create window from application
+	window = application.NewWindow(applicationName)
+	window.Resize(fyne.NewSize(400, 200))
+	// Create a label
+	label = widget.NewLabel("test")
+
+	// Create a button
+	button = widget.NewButton("Testar", buttonOnClick)
+
+	// Set the window content to contain a vertical box which in turn contains the label the button
+	window.SetContent(widget.NewVBox(label, button))
+	window.ShowAndRun()
+}
+
+func buttonOnClick() {
+	label.SetText("asdf")
+}
+
+func ParseFlags() {
 	flag.BoolVar(&daemon, "d", false, "Daemonize instead of showing the full window.")
 	flag.Parse()
-	return daemon
-}
-
-func button_onclick() {
-	dialog, err := gtk.DialogNew()
-	if err != nil {
-		log.Fatal("Unable to create dialog:", err)
-	}
-	dialogText, err := Entry.GetText()
-	if err != nil {
-		log.Fatal("Unable to get text from entry:", err)
-	}
-	dialog.SetTitle(dialogText)
-	dialog.SetDefaultSize(200, 100)
-	dialog.Show()
 }
 
 func onReady() {
-	systray.SetIcon(icon.Data)
-	systray.SetTitle("Awesome App")
-	systray.SetTooltip("Pretty awesome")
-	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
-	mChange := systray.AddMenuItem("Change Me", "Change Me")
-	mChecked := systray.AddMenuItem("Unchecked", "Check Me")
-	mEnabled := systray.AddMenuItem("Enabled", "Enabled")
-	// Sets the icon of a menu item. Only available on Mac.
-	mEnabled.SetTemplateIcon(icon.Data, icon.Data)
+	systray.SetIcon(getIcon("bulb.ico"))
+	systray.SetTitle(applicationName)
+	systray.SetTooltip(applicationName)
 
-	systray.AddMenuItem("Ignored", "Ignored")
-
-	subMenuTop := systray.AddMenuItem("SubMenu", "SubMenu Test (top)")
-	subMenuMiddle := subMenuTop.AddSubMenuItem("SubMenu - Level 2", "SubMenu Test (middle)")
-	subMenuBottom := subMenuMiddle.AddSubMenuItem("SubMenu - Level 3", "SubMenu Test (bottom)")
-	subMenuBottom2 := subMenuMiddle.AddSubMenuItem("Panic!", "SubMenu Test (bottom)")
-
-	//mUrl := systray.AddMenuItem("Open UI", "my home")
-
+	mViewWindow := systray.AddMenuItem("Show window", "Show the program window")
 	systray.AddSeparator()
-	shown := true
-	toggle := func() {
-		if shown {
-			subMenuBottom.Check()
-			subMenuBottom2.Hide()
-			mQuit.Hide()
-			mEnabled.Hide()
-			shown = false
-		} else {
-			subMenuBottom.Uncheck()
-			subMenuBottom2.Show()
-			mQuit.Show()
-			mEnabled.Show()
-			shown = true
-		}
-	}
+	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
 
-	for {
-		select {
-		case <-mChange.ClickedCh:
-			mChange.SetTitle("I've Changed")
-		case <-mChecked.ClickedCh:
-			if mChecked.Checked() {
-				mChecked.Uncheck()
-				mChecked.SetTitle("Unchecked")
-			} else {
-				mChecked.Check()
-				mChecked.SetTitle("Checked")
+	go func() {
+		for {
+			select {
+			case <-mViewWindow.ClickedCh:
+				fyneWindow()
+				log.Println("Opening main window")
+			case <-mQuit.ClickedCh:
+				systray.Quit()
+				log.Println("Quitting...")
+				return
 			}
-		case <-mEnabled.ClickedCh:
-			mEnabled.SetTitle("Disabled")
-			mEnabled.Disable()
-		//case <-mUrl.ClickedCh:
-		//	open.Run("https://www.getlantern.org")
-		case <-subMenuBottom2.ClickedCh:
-			panic("panic button pressed")
-		case <-subMenuBottom.ClickedCh:
-			toggle()
-		//case <-mToggle.ClickedCh:
-		//	toggle()
-		case <-mQuit.ClickedCh:
-			systray.Quit()
-			fmt.Println("Quit2 now...")
-			return
 		}
+	}()
+}
+
+func getIcon(s string) []byte {
+	b, err := ioutil.ReadFile(s)
+	if err != nil {
+		fmt.Print(err)
 	}
+	return b
 }
 
 func onExit() {
